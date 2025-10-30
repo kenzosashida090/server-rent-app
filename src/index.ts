6da118b0625c00636f544116ab04a8e9c1c99446
kenzosashida090 <kenzosashida1@gmail.com>
2025-10-30 13:41:28 -0600

    [new] Adding the express, prisma typescript boilerplate

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { type Request, type Response } from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import morgan from "morgan"
+import helmet from "helmet"
+
+
+// Config
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors())
+
+app.get("/", (req:Request, res:Response)=>{
+    res.send("This sucks")
+})
+const port = process.env.PORT || 3002
+app.listen(port, 
+    ()=>{
+        console.log(`Server running on port ${port}`)
+    }
+)
