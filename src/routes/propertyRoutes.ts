d777c35c6914f91fea1f502f71099f213d50231f
kenzosashida090 <kenzosashida1@gmail.com>
2026-03-23 14:29:20 -0600

    [new] Leases proeprties service

--- src/routes/propertyRoutes.ts
+++ src/routes/propertyRoutes.ts
@@ -2,11 +2,13 @@ import express from "express"
 import { authMiddleware } from "../middleware/authMiddleware.js"
 import {getProperties, createProperty, getProperty} from "../controllers/propertyControllers.js"
 import multer from "multer"
+import { getManagerPropertiesLeases } from "../controllers/managerControllers.js"
 const router = express.Router()
 const storage = multer.memoryStorage()
 const upload = multer({storage})
 router.get("/", getProperties)
 router.get("/:id", getProperty)
-router.post("/", authMiddleware(["manager"]), upload.array("photos  ")  ,createProperty)
+router.post("/", authMiddleware(["manager"]), upload.array("photos")  ,createProperty)
+router.get("/:id/leases", getManagerPropertiesLeases)
 
 export default router
