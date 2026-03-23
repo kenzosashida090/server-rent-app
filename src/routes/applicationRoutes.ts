37c628c84e321e73543158408a68581f645473d4
kenzosashida090 <kenzosashida1@gmail.com>
2026-03-23 14:28:47 -0600

    [fix] application bad typo method

--- src/routes/applicationRoutes.ts
+++ src/routes/applicationRoutes.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "../middleware/authMiddleware.js"
 import { createApplication, getApplicationsList, updateApplication } from "../controllers/applicationControllers.js"
 const router = express.Router()
 
-router.get("/", authMiddleware(["tenant"]), createApplication)
+router.post("/", authMiddleware(["tenant"]), createApplication)
 router.put("/:id/status", authMiddleware(["manager", "tenant"]), updateApplication)
 router.get("/", authMiddleware(["tenant","manager"]), getApplicationsList)
 
