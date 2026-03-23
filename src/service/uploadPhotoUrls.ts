8766f1794263c7c1e68d4abd245016ef03b0f395
kenzosashida090 <kenzosashida1@gmail.com>
2026-03-23 14:31:03 -0600

    [new] Credentials for aws storage

--- src/service/uploadPhotoUrls.ts
+++ src/service/uploadPhotoUrls.ts
@@ -1,11 +1,16 @@
 import { S3Client } from "@aws-sdk/client-s3"
 import { Upload } from "@aws-sdk/lib-storage"
-
+import 'dotenv/config'; 
 const s3Client = new S3Client(
     {
         region: process.env.AWS_REGION!,
+        credentials:{
+             accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        }
     }
 )
+
 export const UploadPhotosS3 = async(files: Express.Multer.File[] ) =>(
     await Promise.all(
         files.map(async (file)=>{
